b14112789f247b360b68ab80bd726d23cfa95cc4
N.Raju <151713861+NalagamdinniRaju@users.noreply.github.com>
2025-02-26 10:26:37 +0530

    Update doctorRoutes.js

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -25,60 +25,6 @@ router.get("/:id", async (req, res) => {
     }
   });
 
-// router.get("/:id/slots", async (req, res) => {
-//   try {
-//     const doctor = await Doctor.findById(req.params.id)
-//     if (!doctor) {
-//       return res.status(404).json({ message: "Doctor not found" })
-//     }
-
-//     const date = parseISO(req.query.date)
-//     const startTime = setMinutes(
-//       setHours(date, Number.parseInt(doctor.workingHours.start.split(":")[0])),
-//       Number.parseInt(doctor.workingHours.start.split(":")[1]),
-//     )
-//     const endTime = setMinutes(
-//       setHours(date, Number.parseInt(doctor.workingHours.end.split(":")[0])),
-//       Number.parseInt(doctor.workingHours.end.split(":")[1]),
-//     )
-
-//     const appointments = await Appointment.find({
-//       doctorId: doctor._id,
-//       date: {
-//         $gte: startTime,
-//         $lt: endTime,
-//       },
-//     })
-
-//     const slots = []
-//     let currentSlot = startTime
-
-//     while (currentSlot < endTime) {
-//       const slotEnd = addMinutes(currentSlot, 30)
-//       const isBooked = appointments.some(
-//         (appointment) =>
-//           isWithinInterval(currentSlot, {
-//             start: appointment.date,
-//             end: addMinutes(appointment.date, appointment.duration),
-//           }) ||
-//           isWithinInterval(slotEnd, {
-//             start: appointment.date,
-//             end: addMinutes(appointment.date, appointment.duration),
-//           }),
-//       )
-
-//       if (!isBooked) {
-//         slots.push(format(currentSlot, "HH:mm"))
-//       }
-
-//       currentSlot = slotEnd
-//     }
-
-//     res.json(slots)
-//   } catch (error) {
-//     res.status(500).json({ message: error.message })
-//   }
-// })
 router.get("/:id/slots", async (req, res) => {
     try {
       const doctor = await Doctor.findById(req.params.id);
